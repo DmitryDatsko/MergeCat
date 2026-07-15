ed27af41ba028705507cf566d3e307b985a57c41
Dmytro <dmitrydatsko.of@gmail.com>
2026-07-15 16:07:29 +0100

    new migrations, map inbound claims set to false

--- Program.cs
+++ Program.cs
@@ -27,6 +27,7 @@ builder
     .Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
     {
+        options.MapInboundClaims = false;
         options.TokenValidationParameters = new TokenValidationParameters
         {
             ValidateIssuer = true,
